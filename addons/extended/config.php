f4ce579a28aa662e70e286fea0fba797e7576785
Matthew Larkin <matthew@matthewlarkin.info>
2021-03-07 21:32:32 -0500

    Updates extended to not require extended users

--- addons/extended/config.php
+++ addons/extended/config.php
@@ -5,7 +5,7 @@
     ///////////EDIT BELOW THIS////////////////////////////////////////
 
     // Names of the required templates
-    $t['users_tpl'] = 'accounts.php';
-    $t['login_tpl'] = 'login.php';
-    $t['lost_password_tpl'] = 'lost-password.php';
-    $t['registration_tpl'] = 'register.php';
+    // $t['users_tpl'] = 'accounts.php';
+    // $t['login_tpl'] = 'login.php';
+    // $t['lost_password_tpl'] = 'lost-password.php';
+    // $t['registration_tpl'] = 'register.php';
